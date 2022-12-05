4dbdc07aa3b6fc3c4207b5f5d3a68893b42f1bca
Caleb Navarrro <calebnavarro@ufl.edu>
2022-12-05 14:55:53 -0500

    updated version

--- src/unordered map.cpp
+++ src/unordered map.cpp
@@ -1,4 +1,3 @@
-
 #include <string>
 #include <sstream>
 #include <iostream>
@@ -7,90 +6,172 @@
 #include <vector>
 using namespace std;
 
-class unordered_map{
-  private:
-    list <pair<int,string>> *buckets;
-    int bucket_size;
-    int total_elements;
-    float max_load_factor;
+template <typename K, typename V>
+  
 
-    int hash(int key){
-      return key % 10;
+class HashNode {
+public:
+    V value;
+    K key;
+  
+   
+    HashNode(K key, V value)
+    {
+        this->value = value;
+        this->key = key;
     }
-
-public: 
-
-unordered_map(int n){
-  bucket_size = n;
-  buckets = new list<pair<int,string>>[bucket_size];
-  total_elements = 0;
-  max_load_factor = 0.75;
-}
-
-void insert(int key, string val){
-  buckets[hash(key)].push_back({key,val});
-  total_elements++;
-  rehash();
-
-
-}
-
-
-void rehash(){
-  if(total_elements/bucket_size <= max_load_factor){
-    return;
-  }
-  bucket_size *= 2;
-  auto new_buckets = new list<pair<int,string>>[bucket_size];
-  for(int i = 0; i < bucket_size/2; i++){
-    for(auto& itr: buckets[i]){
-      new_buckets[hash(itr.first)].push_back(itr);
-    }
-  }
-  delete[] buckets;
-  buckets = new_buckets;
-}
-
- vector <pair<int,string>> find(int key){
-  pair<int,string> prev1;
-  pair<int,string> prev2;
-  vector <pair<int,string>> ans;
-
-  int x = hash(key);
-   for(auto& i : *buckets){
-     if (prev1.first < key){
-      prev1 = i;
-
-     }else if (prev1.first >= key){
-      ans.push_back(prev1);
-      ans.push_back(prev2);
-      break;
-     
-     } if(prev2.first < key){
-      prev2 =i;
-     }
-     
-   }
-  return ans;
- }
-
-
-
 };
+  
 
+template <typename K, typename V>
+  
+
+class HashMap {
+    
+    HashNode<K, V>** arr;
+    int capacity;
+    
+    int size;
+    
+    
+  
+public:
+    HashMap()
+    {
+        
+        capacity = 20;
+        size = 0;
+        arr = new HashNode<K, V>*[capacity];
+  
+        
+        for (int i = 0; i < capacity; i++)
+            arr[i] = NULL;
+  
+        
+        
+    }
+    
+    int hashCode(K key)
+    {
+        return key % capacity;
+    }
+  
+    // Function to add key value pair
+    void insertNode(K key, V value)
+    {
+        HashNode<K, V>* temp = new HashNode<K, V>(key, value);
+  
+        
+        int hashIndex = hashCode(key);
+  
+        // find next free space
+        while (arr[hashIndex] != NULL
+               && arr[hashIndex]->key != key
+               && arr[hashIndex]->key != -1) {
+            hashIndex++;
+            hashIndex %= capacity;
+        }
+  
+        // if new node to be inserted
+        // increase the current size
+        if (arr[hashIndex] == NULL|| arr[hashIndex]->key == -1)
+            size++;
+        arr[hashIndex] = temp;
+    }
+  
+   
+    
+  
+    
+    vector<V> get(int key)
+    { 
+       HashNode<K, V>* temp1 = new HashNode<K, V>(0, "");
+       HashNode<K, V>* temp2 = new HashNode<K, V>(0, "");
+        V prev1;
+        V prev2;
+        vector <V> ans;
+        bool test= true;
+       
+         
+        
+        int count =0;
+        int counter = 0;
+  
+        
+        while(test) { 
+            if (arr[count] != NULL){
+                counter++;
+            
+                if (temp1->key < arr[count]->key && temp1->key < hashCode(key)){
+                    temp1 = arr[count];
+                }
+                    
+                   
+                cout<<counter<<endl;
+            if (counter == size){
+                cout <<size<<endl;
+                test=false;
+                }
+            }
+        count++;
+        }
+        ans.push_back(temp1->value);
+        
+        
+        return ans;
+    
+    }
+  
+    // Return current size
+    int sizeofMap()
+    {
+        return size;
+    }
+  
+    // Return true if size is 0
+    bool isEmpty()
+    {
+        return size == 0;
+    }
+  
+    // Function to display the stored key value pairs
+     void display()
+    {
+        for (int i = 0; i < capacity; i++) {
+            if (arr[i] != NULL && arr[i]->key != -1)
+                cout << "key = " << arr[i]->key
+                     << "  value = "
+                     << arr[i]->value << endl;
+        }
+    }
+};
 int main()
 {
-  vector <pair<int,string>> answer;
-  unordered_map map(3);
-    map.insert(0,"caleb");
-    map.insert(1,"anna");
-    map.insert(2,"chandler");
-    answer = map.find(3);
-    for(int i=0; i <=answer.size()-1; i++){
-      cout << answer[i].second <<endl;
+  
+  HashMap<int, string>* h = new HashMap<int, string>;
+    h->insertNode(1, "caleb");
+    h->insertNode(5, "chandler");
+    h->insertNode(3, "anna");
+    h->insertNode(4, "brandon");
+    h->insertNode(2, "ashley");
+    h->insertNode(8, "madeline");
+    h->insertNode(13, "bob");
+    h->insertNode(7, "ryan");
+    h->insertNode(9, "catherine");
+    h->insertNode(12, "korynn");
+    h->insertNode(11, "abbey");
+    h->insertNode(10, "ryan");
+    h->insertNode(17, "jake");
+
+
+
+
+
+
+    vector<string> answer = h->get(7);
+    for (int i=0; i<answer.size(); i++){
+        cout<<answer[i]<< endl;
     }
-
-
     
     
     
