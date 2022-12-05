bd24cdea66fe355b63cdaa9367fdd7c0a6686bf8
Caleb Navarrro <calebnavarro@ufl.edu>
2022-12-04 19:39:06 -0500

    updated

--- src/unordered map.cpp
+++ src/unordered map.cpp
@@ -3,79 +3,94 @@
 #include <sstream>
 #include <iostream>
 #include <list>
+#include <ostream>
+#include <vector>
 using namespace std;
 
 class unordered_map{
   private:
-    list <pair<int,int>> *buckets;
+    list <pair<int,string>> *buckets;
     int bucket_size;
     int total_elements;
     float max_load_factor;
 
+    int hash(int key){
+      return key % 10;
+    }
+
 public: 
 
-
-
-
-int hash(int key) {
-  int hashed_key = strongHashingFunction(key);
-  return hashed_key;
+unordered_map(int n){
+  bucket_size = n;
+  buckets = new list<pair<int,string>>[bucket_size];
+  total_elements = 0;
+  max_load_factor =0.75;
 }
 
-int strongHashingFunction(int key){
- return key % 10;
-}
-
-list<int>::iterator find(int key){
-  for(auto& i : buckets[hash(key)]) {
-    if(i->first == key){
-      return i;
-    }
-  }
-  return buckets[hashed_key].end();
-}
-
-
-void insert(int key, int val){
-  auto it = find(key);
-  if(it != buckets[hash(key)].end()){
-    it->second = val;
-    return;
-  }
+void insert(int key, string val){
   buckets[hash(key)].push_back({key,val});
   total_elements++;
-  rehashIfNeeded();
-}
+  rehash();
 
-void delete(int key){
-  auto it = find(key)
-  if(it != buckets[hash(key)].end()){
-    buckets[hash(key)].erase(it);
-    total_elements--;
-  }
+
 }
 
 
-void rehashIfNeeded(){
+void rehash(){
   if(total_elements/bucket_size <= max_load_factor){
     return;
   }
   bucket_size *= 2;
-  auto new_buckets = new list<pair<int,int>>[bucket_size];
+  auto new_buckets = new list<pair<int,string>>[bucket_size];
   for(int i = 0; i < bucket_size/2; i++){
     for(auto& itr: buckets[i]){
-      new_buckets[hash(itr->first)].push_back(*itr);
+      new_buckets[hash(itr.first)].push_back(itr);
     }
   }
   delete[] buckets;
   buckets = new_buckets;
 }
 
+ vector <pair<int,string>> find(int key){
+  pair<int,string> prev1;
+  pair<int,string> prev2;
+  vector <pair<int,string>> ans;
+
+  int x = hash(key);
+   for(auto& i : *buckets){
+     if (prev1.first < key){
+      prev1 = i;
+
+     }else if (prev1.first >= key){
+      ans.push_back(prev1);
+      ans.push_back(prev2);
+      break;
+     
+     } if(prev2.first < key){
+      prev2 =i;
+     }
+     
+   }
+  return ans;
+ }
+
+
+
 };
 
 int main()
 {
-    
+  vector <pair<int,string>> answer;
+  unordered_map map(3);
+    map.insert(0,"caleb");
+    map.insert(1,"anna");
+    map.insert(2,"chandler");
+    answer = map.find(3);
+    for(int i=0; i <=answer.size()-1; i++){
+      cout << answer[i].second <<endl;
+    }
+
+
     
     
     
