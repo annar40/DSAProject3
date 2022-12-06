d923f6959abc882d03f176fc7d2e1d9262ad8db1
annar40 <80236565+annar40@users.noreply.github.com>
2022-12-05 20:31:49 -0500

    print is working YYAYAYAYAYA

--- src/unordered map.cpp
+++ src/unordered map.cpp
@@ -99,8 +99,14 @@ public:
     {
         HashNode<K, V> *temp1 = new HashNode<K, V>(0, "");
         HashNode<K, V> *temp2 = new HashNode<K, V>(0, "");
+        HashNode<K, V> *temp3 = new HashNode<K, V>(100000000, "");
+        HashNode<K, V> *temp4 = new HashNode<K, V>(100000000, "");
+
         V prev1;
         V prev2;
+        V post1;
+        V post2;
+
         vector<V> ans;
         bool test = true;
 
@@ -114,7 +120,7 @@ public:
                 if (counter < size)
                 {
                     counter++;
-                    cout << temp1->key << endl;
+                    // cout << temp1->key << endl;
                     cout << "value in arr " << arr[count]->key << endl;
 
                     if (arr[count]->key >= temp1->key && arr[count]->key < userInputData)
@@ -126,6 +132,16 @@ public:
                         }
                         temp1 = arr[count];
                     }
+                    else if (arr[count]->key < temp3->key && arr[count]->key > userInputData)
+                    {
+
+                        temp3 = arr[count];
+                    }
+                    if(arr[count]->key > temp3->key &&  arr[count]->key < temp4->key)
+                    {
+                        temp4 = arr[count];
+                    }
+                
                 }
                 else
                 {
@@ -137,12 +153,19 @@ public:
         }
         prev1 = temp1->value;
         prev2 = temp2->value;
+        post1 = temp3->value;
+        post2 = temp4->value;
+
         ans.push_back(prev2);
         ans.push_back(prev1);
-
-        cout << "temp1 value: " << prev1<< endl;
-        cout << "temp2 value: " << prev2 << endl;
-
+        // ans.push_back(userInputData);
+        ans.push_back(post1);
+        ans.push_back(post2);
+        
+        // cout << "temp1 value: " << prev1<< endl;
+        // cout << "temp2 value: " << prev2 << endl;
+        // cout << "temp3 value: " << temp3->value << endl;
+        // cout << "temp4 value: " << temp4->value << endl;
         return ans;
     }
 
@@ -169,21 +192,22 @@ public:
                      << arr[i]->value << endl;
         }
     }
+
 };
 int main()
 {
 
     HashMap<int, string> *h = new HashMap<int, string>(20);
-    h->insertNode(1, "caleb");
-    h->insertNode(5, "chandler");
-    h->insertNode(3, "anna");
-    h->insertNode(4, "brandon");
-    h->insertNode(2, "ashley");
-    h->insertNode(8, "madeline");
-    h->insertNode(13, "bob");
-    h->insertNode(7, "ryan");
-    h->insertNode(9, "catherine");
-    h->insertNode(12, "korynn");
+    // h->insertNode(1, "caleb");
+    // h->insertNode(5, "chandler");
+    // h->insertNode(3, "anna");
+    // h->insertNode(4, "brandon");
+    // h->insertNode(2, "ashley");
+    // h->insertNode(8, "madeline");
+    // h->insertNode(13, "bob");
+    // h->insertNode(7, "ryan");
+    // h->insertNode(9, "catherine");
+    // h->insertNode(12, "korynn");
     h->insertNode(11, "abbey");
     h->insertNode(17, "jake");
     h->insertNode(19, "class");
@@ -197,12 +221,16 @@ int main()
     h->insertNode(67, "sea");
     h->insertNode(98, "snake");
 
-    h->display();
+    // h->display();
 
     vector<string> answer = h->get(20);
-    // for (int i=0; i<answer.size(); i++){
-    //     cout<<answer[i]<< endl;
-    // }
+    for (int i=0; i<answer.size(); i++){
+        if(i == 3)
+        {
+            cout << "Your Value: ";
+        }
+        cout<<answer[i] << " ";
+    }
 
     return 0;
 }
