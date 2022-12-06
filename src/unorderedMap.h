238bcf58e21e66ea58b61b9ec8312e594d4ad638
Caleb Navarrro <calebnavarro@ufl.edu>
2022-12-05 21:03:27 -0500

    print function added

--- src/unorderedMap.h
+++ src/unorderedMap.h
@@ -120,7 +120,7 @@ public:
                 if (counter < size)
                 {
                     counter++;
-                    // cout << temp1->key << endl;
+                    
                     cout << "value in arr " << arr[count]->key << endl;
 
                     if (arr[count]->key >= temp1->key && arr[count]->key < userInputData)
@@ -158,14 +158,11 @@ public:
 
         ans.push_back(prev2);
         ans.push_back(prev1);
-        // ans.push_back(userInputData);
+        
         ans.push_back(post1);
         ans.push_back(post2);
         
-        // cout << "temp1 value: " << prev1<< endl;
-        // cout << "temp2 value: " << prev2 << endl;
-        // cout << "temp3 value: " << temp3->value << endl;
-        // cout << "temp4 value: " << temp4->value << endl;
+        
         return ans;
     }
 
@@ -180,45 +177,16 @@ public:
     {
         return size == 0;
     }
+    void print(int search){
+        vector<string> answer = get(search);
+        
+    for (int i=0; i<answer.size(); i++){
+        if(i == 3)
+        {
+            cout << "Your Value: "<< search<<" ";
+        }
+        cout<<answer[i] << " ";
+    }
+    }
 
 };
-// int main()
-// {
-
-//     HashMap<int, string> *h = new HashMap<int, string>(20);
-//     // h->insertNode(1, "caleb");
-//     // h->insertNode(5, "chandler");
-//     // h->insertNode(3, "anna");
-//     // h->insertNode(4, "brandon");
-//     // h->insertNode(2, "ashley");
-//     // h->insertNode(8, "madeline");
-//     // h->insertNode(13, "bob");
-//     // h->insertNode(7, "ryan");
-//     // h->insertNode(9, "catherine");
-//     // h->insertNode(12, "korynn");
-//     h->insertNode(11, "abbey");
-//     h->insertNode(17, "jake");
-//     h->insertNode(19, "class");
-//     h->insertNode(19, "math");
-//     h->insertNode(20, "soccer");
-//     h->insertNode(21, "mom");
-//     h->insertNode(6, "dad");
-//     h->insertNode(14, "hero");
-//     h->insertNode(39, "blah");
-//     h->insertNode(40, "green");
-//     h->insertNode(67, "sea");
-//     h->insertNode(98, "snake");
-
-//     // h->display();
-
-//     vector<string> answer = h->get(20);
-//     for (int i=0; i<answer.size(); i++){
-//         if(i == 3)
-//         {
-//             cout << "Your Value: ";
-//         }
-//         cout<<answer[i] << " ";
-//     }
-
-//     return 0;
-// }
